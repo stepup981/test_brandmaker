3d492cb639eb351a00124b86612a3fc621ef0d26
Roman Berdnikov <berdnikov.roma@gmail.com>
2023-09-28 14:59:10 +0300

    work

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const imgList = [
+  {id: 0, src: './assets/picture/first.png'},
+  {id: 1, src: './assets/picture/second.png'},
+  {id: 2, src: './assets/picture/third.png'},
+  {id: 3, src: './assets/picture/fourth.png'},
+];
+
+const container = document.querySelector('.office__images');
+
+imgList.forEach((item) => {
+  const img = document.createElement('img');
+  img.classList.add('office__img'); // Добавьте имя класса, если необходимо
+  img.src = item.src;
+  container.appendChild(img);
+});
